e52d729aae2c0c7a76863aa3e04de1576e1f8885
unknown <ymj0837@gmail.com>
2024-11-21 15:20:06 +0900

    Initial commit for Lambda deployment

---
+++ app.py
@@ -0,0 +1,26 @@
+import json
+
+from flask import Response
+
+
+def lambda_handler(event, context):
+    """
+    HTTP 요청을 처리하는 AWS Lambda 함수
+    """
+
+    # 들어오는 이벤트 로그
+    print("Event:", event)
+
+    # 응답 메시지
+    response = {
+        "statusCode": 200,
+        "headers":{
+            "Content-Type":"application/json"
+        },
+        "body":json.dumps({
+            "message":"Hello from AWS Lambda!",
+            "input": event
+        })
+    }
+
+    return response
